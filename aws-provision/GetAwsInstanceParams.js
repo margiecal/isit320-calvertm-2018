7a64afe46be5723412c3abb54bd65d597a2500fd
Margie on her Samsung <margie@ESLQueen.com>
2018-10-08 17:09:02 -0700

    why not use the program

---
+++ aws-provision/GetAwsInstanceParams.js
@@ -0,0 +1,53 @@
+var AWS = require('aws-sdk');
+
+const awsParams = {};
+
+const getInstanceParams = (awsParams) => {
+    return {
+        BlockDeviceMappings: [
+            {
+                DeviceName: "/dev/sda1",
+                Ebs: {
+                    VolumeSize: 16,
+                    VolumeType: 'gp2'
+                }
+            }
+        ],
+        ImageId: 'ami-702d7f08',
+        InstanceType: 't2.micro',
+        KeyName: 'EC2Fall2018',
+        SecurityGroupIds: ['sg-0d6dd07943b766627'],
+        MinCount: 1,
+        MaxCount: 1
+    };
+};
+
+function showAwsEducateCredentials() {
+    AWS.config.credentials.get(function () {
+        var accessKeyId = AWS.config.credentials.accessKeyId;
+        var secretAccessKey = AWS.config.credentials.secretAccessKey;
+        console.log("Access Key:", AWS.config.credentials.accessKeyId);
+        console.log("Secret Access Key:", AWS.config.credentials.secretAccessKey);
+    });
+}
+
+function showAwsCharlieConfiguration() {
+    AWS.config.loadFromPath(process.env.HOME + '/.aws/config.json');
+    console.log(AWS.config);
+}
+
+
+module.exports.awsCharlie= () => {
+    showAwsCharlieConfiguration();
+    awsParams.ImageId = 'ami-0bbe6b35405ecebdb';
+    awsParams.KeyName = 'EC2Fall2018';
+    awsParams.SecurityGroupIds = ['sg-0d6dd07943b766627'];
+    return getInstanceParams(awsParams);
+};
+
+
+
+
+
+
+
