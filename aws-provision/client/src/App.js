cc4ef0e1833f6341a91b473f7a0d7c1350499a3d
margie@home <margie@home.com>
2018-10-21 18:16:08 -0700

    seems to be working

--- aws-provision/client/src/App.js
+++ aws-provision/client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 
@@ -45,10 +44,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-            <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>Welcome to React</h2>
-        </div>
+
 
         <p className="App-intro">
             state: {this.state.status} file: {this.state.file}
