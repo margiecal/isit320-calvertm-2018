71969ff801e82fef71977c2d0f82780734c27d29
Margie Calvert <margie@school.com>
2018-10-22 17:28:32 -0700

    now it is showing the data

--- week06-run-ssh/client/src/App.js
+++ week06-run-ssh/client/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        };
+    }
     callCpuInfo = () => {
         const that = this;
         fetch('/call-cpu-info')
@@ -9,12 +16,12 @@ class App extends Component {
                 return response.json();
             })
             .then(function (json) {
-                console.log('parsed json', json);
-                that.setState(foo => (json));
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
             })
             .catch(function (ex) {
                 console.log('parsing failed, URL bad, network down, or similar', ex);
-            });
+            })
     };
 
   render() {
@@ -26,6 +33,7 @@ class App extends Component {
       </header>
         <main>
             <button onClick={this.callCpuInfo}>Call Cpu Info</button>
+            <text>{this.state.allData}</text>
         </main>
 
           <footer>
