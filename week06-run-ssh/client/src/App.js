274e7feb64df3ba03f9987827780e64aa9d885ff
margie@home <margie@home.com>
2018-10-28 18:10:39 -0700

    this is a working run-ssh

---
+++ week06-run-ssh/client/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        };
+    }
+    callCpuInfo = () => {
+        const that = this;
+        fetch('/call-cpu-info')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
+            })
+            .catch(function (ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            })
+    };
+
+  render() {
+    return (
+      <div className="App">
+      <header>
+          <p className="byline">by </p>
+        <h1>Run SSH</h1>
+      </header>
+        <main>
+            <button onClick={this.callCpuInfo}>Call Cpu Info</button>
+            <text>{this.state.allData}</text>
+        </main>
+
+          <footer>
+              <p>&copy; 2018 </p>
+          </footer>
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
