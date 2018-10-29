aedfd91dd3966f73c87b6850075062f5fff1a3dd
margie@home <margie@home.com>
2018-10-29 00:00:24 -0700

    working copy of systemcheck

---
+++ week06-system-check/server/public/precache-manifest.dc0136470f9dde90d27a6af37f4b4ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "800d128220817b875031",
+    "url": "/static/js/main.800d1282.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "800d128220817b875031",
+    "url": "/static/css/main.693a1b63.chunk.css"
+  },
+  {
+    "revision": "a63c6acceb0009f209e3aa5d5636b8dc",
+    "url": "/index.html"
+  }
+];
