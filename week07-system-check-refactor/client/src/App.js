0b2ba83e281362989607a8839358dd8278030daf
Margie Calvert <margie@school.com>
2018-11-05 17:14:16 -0800

    got ElfHeader and App and ElfHeader.test and App.test

--- week07-system-check-refactor/client/src/App.js
+++ week07-system-check-refactor/client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ElfHeader from './ElfHeader';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header>
-                    <h1>System Check</h1>
-                </header>
+                <ElfHeader />
             </div>
         );
     }
