bd9b02e93e466a05be74db488cf1c2a9803930fc
Margie Calvert <margie@school.com>
2018-11-05 18:23:25 -0800

    this is now working with copy command

--- week07-system-check-refactor/client/src/App.js
+++ week07-system-check-refactor/client/src/App.js
@@ -4,10 +4,34 @@ import './App.css';
 import ElfHeader from './ElfHeader';
 
 class App extends Component {
+
+    copyFile = () => {
+        const that = this;
+        fetch('/script-pusher/copy-file')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
     render() {
         return (
             <div className="App">
                 <ElfHeader />
+
+                <main>
+                    <button onClick={this.copyFile}>Copy File</button>
+
+                </main>
+                <footer>
+                    <p>&copy; by Charlie Calvert </p>
+                </footer>
             </div>
         );
     }
