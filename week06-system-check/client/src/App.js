de43e86dd313355aca11e8bd948be6a34d6d2ac7
margie@home <margie@home.com>
2018-11-17 14:12:23 -0800

    working with copy file and run cpu info

--- week06-system-check/client/src/App.js
+++ week06-system-check/client/src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        };
+    }
+    callCpuInfo = () => {
+        const that = this;
+        fetch('/ssh-runner/call-cpu-info')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
+            })
+            .catch(function (ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            })
+    };
+
     copyFile = () => {
         const that = this;
         fetch('/script-pusher/copy-file')
@@ -19,15 +41,21 @@ class App extends Component {
             });
     };
 
+
+
+
     render() {
         return (
             <div className="App">
                 <header>
-                    <h1>Run CPU Info </h1>
+                    <h1>Week6 SystemCheck </h1>
                 </header>
 
                 <main>
-                    <button onClick={this.copyFile}>RunCpuInfo</button>
+                    <button onClick={this.copyFile}>Copy File</button>
+                    <button onClick={this.callCpuInfo}>Run CPU Info</button>
+                    <p>{this.state.allData}</p>
+
                 </main>
                 <footer>
                     <p>&copy; by Margie Calvert </p>
