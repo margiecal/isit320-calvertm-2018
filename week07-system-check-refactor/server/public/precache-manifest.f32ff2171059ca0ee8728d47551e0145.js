c45095bd4b3cf8f0431d9357a2e09a96b611be72
margie@home <margie@home.com>
2018-11-19 14:35:48 -0800

    still working on the build stuff this is not a functioning versioncheck

---
+++ week07-system-check-refactor/server/public/precache-manifest.f32ff2171059ca0ee8728d47551e0145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "495644357a31727dacf5",
+    "url": "/static/js/main.49564435.chunk.js"
+  },
+  {
+    "revision": "bb1bdfcd068ff2ea51ca",
+    "url": "/static/js/1.bb1bdfcd.chunk.js"
+  },
+  {
+    "revision": "495644357a31727dacf5",
+    "url": "/static/css/main.e8808f5e.chunk.css"
+  },
+  {
+    "revision": "ceafcf63cba80b3e313d50280b3dd16a",
+    "url": "/index.html"
+  }
+];
