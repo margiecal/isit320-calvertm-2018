606394784b7c022ef8dafa921e4e225910a8b817
margie@home <margie@home.com>
2018-11-18 16:45:58 -0800

    this is my working copy with my build script

--- week07-system-check-refactor/client/src/App.js
+++ week07-system-check-refactor/client/src/App.js
@@ -9,6 +9,26 @@ class App extends Component {
             allData: 'unknown'
         };
     }
+
+    handleChange = (event) => {
+        const selectedValue = event.target.value;
+        console.log('HANDLE CHANGE', selectedValue);
+        this.setState({
+            ...this.state,
+            selectedValue: selectedValue
+        });
+
+    };
+
+    handleSubmit= (event) => {
+        this.setState({allData: ''});
+        console.log('A name was submitted: ' , this.state);
+        //if (this.state.selectedValue === 'cpu') {
+        this.runCpuInfo(this.state.selectedValue);
+        //}
+        event.preventDefault();
+    };
+
     callCpuInfo = () => {
         const that = this;
         fetch('/ssh-runner/call-cpu-info')
@@ -45,16 +65,43 @@ class App extends Component {
     };
 
     render() {
+        const radioWeb =  (
+            <div className="container">
+                <form onSubmit={this.handleSubmit} >
+
+                    <div className="elf-form-field" >
+                        <input type="radio" name="app-choice" value="CpuInfo" id="elf-radio-cpu" onChange={this.handleChange}/>
+                        <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+                        <input type="radio" name="app-choice" value="VersionCheck" id="elf-radio-version" onChange={this.handleChange}/>
+                        <label htmlFor="elf-radio-version">Version Info</label>
+                    </div>
+
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Run System Script</button>
+                    </div>
+                </form>
+            </div>
+        );
+
+
         return (
             <div className="App">
                 <ElfHeader />
 
+                <section>
+                    {radioWeb}
+                </section>
+
+                <section>
+                    <pre>{this.state.allData}</pre>
+                </section>
 
 
                 <main>
                     <button onClick={this.copyFile}>Copy File</button>
                     <button onClick={this.callCpuInfo}>Run CPU Info</button>
-                    <p>{this.state.allData}</p>
+
                 </main>
                 <footer>
                     <p>&copy; by Margie Calvert </p>
---
+++ week07-system-check-refactor/server/public/precache-manifest.de698260caa2e615da00c150fea9c4ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c810408b58a92d4d3f21",
+    "url": "/static/js/main.c810408b.chunk.js"
+  },
+  {
+    "revision": "45af52f1181cf8191db9",
+    "url": "/static/js/1.45af52f1.chunk.js"
+  },
+  {
+    "revision": "c810408b58a92d4d3f21",
+    "url": "/static/css/main.219c8f29.chunk.css"
+  },
+  {
+    "revision": "95d1058b31b0b4f5dc7e1d3a62033242",
+    "url": "/index.html"
+  }
+];
