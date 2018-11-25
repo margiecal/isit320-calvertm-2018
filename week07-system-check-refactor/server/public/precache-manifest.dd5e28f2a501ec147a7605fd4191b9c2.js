7857494b90e88f76295ec0eb76f56d4530be7866
margie@home <margie@home.com>
2018-11-24 17:17:09 -0800

    this is updating my copyscript and valid systemcheckrefactor

---
+++ week07-system-check-refactor/server/public/precache-manifest.dd5e28f2a501ec147a7605fd4191b9c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c025e57c3398f9c0b703",
+    "url": "/static/js/main.c025e57c.chunk.js"
+  },
+  {
+    "revision": "bb1bdfcd068ff2ea51ca",
+    "url": "/static/js/1.bb1bdfcd.chunk.js"
+  },
+  {
+    "revision": "c025e57c3398f9c0b703",
+    "url": "/static/css/main.e8808f5e.chunk.css"
+  },
+  {
+    "revision": "7a80689749c6d317791efdacf1421786",
+    "url": "/index.html"
+  }
+];
