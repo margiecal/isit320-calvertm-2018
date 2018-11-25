b6b820c17a852714aef50fe8b89e378d9ccb4698
margie@home <margie@home.com>
2018-11-24 22:02:57 -0800

    setting up the midterm

--- midterm/client/src/App.js
+++ midterm/client/src/App.js
@@ -1,146 +1,199 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ElfHeader from './ElfHeader';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.dataEndPoints = ['/script-pusher/run-script?script=',
+            '/script-pusher/run-system-tool?script='];
         this.state = {
-            allData: 'unknown',
-            checkedRadioButton: '',
-            message: ''
+            allData: '',
+            selectedValue: '',
+            endPointIndex: 0
         };
-    }
 
-    callCpuInfo = () => {
-        const that = this;
-        fetch('/ssh-runner/call-cpu-info')
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                console.log('parsed json', json.allData);
-                that.setState({ allData: json.allData });
-            })
-            .catch(function(ex) {
-                console.log(
-                    'parsing failed, URL bad, network down, or similar',
-                    ex
-                );
+        runScript = (path, script) => {
+            const that = this;
+            if (!script) {
+                return;
+            }
+            fetch(path + script)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    console.log('allData', json.allData);
+                    console.log('result', json.result);
+                    console.log('code', json.code);
+                    console.log('error', json.error);
+                    let info = '';
+                    if (json.result === 'error') {
+                        info = json.error;
+                    } else if (script === 'CpuInfo') {
+                        var regex1 = RegExp('model name.*', 'g');
+                        let array1 = regex1.exec(json.allData);
+                        while (array1 !== null) {
+                            info += array1[0] + '\n';
+                            console.log(`Found ${array1[0]}.`);
+                            array1 = regex1.exec(json.allData);
+                        }
+                    } else {
+                        info = json.allData;
+                    }
+                    that.setState({allData: info});
+                })
+                .catch(function (ex) {
+                    console.log('parsing failed, URL bad, network down, or similar', ex);
+                });
+        };
+
+
+        callCpuInfo = () => {
+            const that = this;
+            fetch('/ssh-runner/call-cpu-info')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    console.log('parsed json', json.allData);
+                    that.setState({allData: json.allData});
+                })
+                .catch(function (ex) {
+                    console.log(
+                        'parsing failed, URL bad, network down, or similar',
+                        ex
+                    );
+                });
+        };
+
+        versionCheck = () => {
+            const that = this;
+
+            fetch('/ssh-runner/call-version-check')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    console.log('Version Check AllData', json.allData);
+                    that.setState({allData: json.allData});
+                })
+                .catch(function (ex) {
+                    console.log(
+                        'parsing failed, URL bad, network down, or similar',
+                        ex
+                    );
+                });
+        };
+
+
+        handleChange = (event) => {
+            const selectedValue = event.target.value;
+            const endPointIndex = event.target.getAttribute('data-endpoint');
+            console.log('HANDLE CHANGE', selectedValue);
+            this.setState({
+                ...this.state,
+                selectedValue: selectedValue,
+                endPointIndex: endPointIndex
             });
-    };
 
-    versionCheck = () => {
-        const that = this;
+        };
 
-        fetch('/ssh-runner/call-version-check')
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                console.log('Version Check AllData', json.allData);
-                that.setState({ allData: json.allData });
-            })
-            .catch(function(ex) {
-                console.log(
-                    'parsing failed, URL bad, network down, or similar',
-                    ex
-                );
-            });
-    };
 
-    handleChange = event => {
-        const selectedValue = event.target.value;
-        console.log('HANDLE CHANGE', selectedValue);
-        this.setState({ checkedRadioButton: selectedValue });
-    };
 
-    handleSubmit = event => {
-        this.setState({ allData: '' });
-        console.log('A name was submitted: ', this.state);
-        if (this.state.checkedRadioButton === 'CpuInfo') {
-            this.callCpuInfo();
-        } else if (this.state.checkedRadioButton === 'VersionCheck') {
-            this.versionCheck();
+        handleSubmit = (event) => {
+            this.setState({allData: ''});
+            console.log('A name was submitted: ', this.state);
+            this.runScript(this.dataEndPoints[this.state.endPointIndex],
+                this.state.selectedValue);
+            event.preventDefault();
+        };
+
+
+        copyFile = () => {
+            const that = this;
+            fetch('/script-pusher/copy-file')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (json) {
+                    console.log('parsed json', json);
+                })
+                .catch(function (ex) {
+                    console.log(
+                        'parsing failed, URL bad, network down, or similar',
+                        ex
+                    );
+                });
+        };
+
+        render()
+        {
+            const radioWeb = (
+                <div className="container">
+                    <form onSubmit={this.handleSubmit}>
+                        <fieldset>
+                            <div className="elf-form-field">
+                                <legend>Services</legend>
+                                <input
+                                    type="radio"
+                                    name="app-choice"
+                                    data-endpoint="0"
+                                    value="CpuInfo"
+                                    id="elf-radio-cpu"
+                                    onChange={this.handleChange}
+                                />
+                                <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+                                <input
+                                    type="radio"
+                                    name="app-choice"
+                                    data-endpoint="0"
+                                    value="VersionCheck"
+                                    id="elf-radio-version"
+                                    onChange={this.handleChange}
+                                />
+                                <label htmlFor="elf-radio-version">Version Info</label>
+                            </div>
+
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-primary">
+                                    Run System Script
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            );
+
+            return (
+                <div className="App">
+                    <ElfHeader/>
+
+                    <section>
+                        {radioWeb}
+                        <p>
+                            Selected radio button: {this.state.checkedRadioButton}
+                        </p>
+                    </section>
+
+                    <section>
+                        <pre>{this.state.allData}</pre>
+                    </section>
+
+                    <main>
+                        <button onClick={this.runFoo}>Run Foo</button>
+                        <button onClick={this.copyFile}>Copy File</button>
+                        <button onClick={this.callCpuInfo}>Run CPU Info</button>
+                    </main>
+                    <footer>
+                        <p>&copy; by Margie Calvert </p>
+                    </footer>
+                </div>
+            );
         }
-        event.preventDefault();
-    };
-
-    copyFile = () => {
-        const that = this;
-        fetch('/script-pusher/copy-file')
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                console.log('parsed json', json);
-            })
-            .catch(function(ex) {
-                console.log(
-                    'parsing failed, URL bad, network down, or similar',
-                    ex
-                );
-            });
-    };
-
-    render() {
-        const radioWeb = (
-            <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                    <div className="elf-form-field">
-                        <input
-                            type="radio"
-                            name="app-choice"
-                            value="CpuInfo"
-                            id="elf-radio-cpu"
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor="elf-radio-cpu">CpuInfo</label>
-
-                        <input
-                            type="radio"
-                            name="app-choice"
-                            value="VersionCheck"
-                            id="elf-radio-version"
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor="elf-radio-version">Version Info</label>
-                    </div>
-
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
-                            Run System Script
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-
-        return (
-            <div className="App">
-                <ElfHeader />
-
-                <section>
-                    {radioWeb}
-                    <p>
-                        Selected radio button: {this.state.checkedRadioButton}
-                    </p>
-                </section>
-
-                <section>
-                    <pre>{this.state.allData}</pre>
-                </section>
-
-                <main>
-                    <button onClick={this.copyFile}>Copy File</button>
-                    <button onClick={this.callCpuInfo}>Run CPU Info</button>
-                </main>
-                <footer>
-                    <p>&copy; by Margie Calvert </p>
-                </footer>
-            </div>
-        );
     }
-}
 
-export default App;
+    export
+    default
+    App;
--- midterm/server/routes/script-pusher.js
+++ midterm/server/routes/script-pusher.js
@@ -3,6 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 
+const check = (request, response, next) => {
+    console.log('REQUEST CHECK CALLED', request.query);
+    const validOptions = ['CpuInfo', 'VersionCheck', 'uptime'];
+    if (request.query.script) {
+        console.log('INSIDE REQUEST SCRIPT');
+        if (!validOptions.includes(request.query.script)) {
+            console.log('INSIDE REQUEST INVALID OPTION');
+            response.send({result: 'error', error: 'Invalid Option: ' + request.query.script, script: request.query.script});
+            return;
+        }
+    }
+    next();
+};
+
+router.use(check);
+
+////Following need to be implemented
+router.get('/run-script', (request, response) => {...
+    ///stuff from JsObjects});
+router.get('/run-system-tool', (request, response) => {..
+   ///stuff from the /usr/bin directory on our system.});
+
+    //on client this.dataEndPoints = ['/script-pusher/run-script?script=',
+    // '/script-pusher/run-system-tool?script='];
+
+    /*Calls to /script-pusher/run-script run code from JsObjects.
+    Calls to /script-pusher/run-system-tool run system utilities.
+    It's up to you to see how this simple array is used in the program
+    to help sort out this problem.*/
+
+//////Above need to be implemented
+
 const spawn = require('child_process').spawn;
 
 let allData = '';
