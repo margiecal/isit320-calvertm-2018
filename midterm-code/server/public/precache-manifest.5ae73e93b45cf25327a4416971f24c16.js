855257a58bf76b73a4aebb2943e4db9f2c89b1d7
margie@home <margie@home.com>
2018-12-04 22:00:09 -0800

    built the build to run on server

---
+++ midterm-code/server/public/precache-manifest.5ae73e93b45cf25327a4416971f24c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7107fa7464a1a5d1fc18",
+    "url": "/static/css/main.cf1d6699.chunk.css"
+  },
+  {
+    "revision": "7107fa7464a1a5d1fc18",
+    "url": "/static/js/main.7107fa74.chunk.js"
+  },
+  {
+    "revision": "26a0146c423ef4a965ff",
+    "url": "/static/js/1.26a0146c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b10a07fc470b104c6a8f3947c9384506",
+    "url": "/index.html"
+  }
+];
